b6a5c6938e875f1b8a2a79526919df1c2e17facf
Vadzim Shuts <vadzim_shuts@epam.com>
2020-05-31 19:10:41 +0300

    fix: fix user reducer

--- src/redux/reducers/user.reducer.js
+++ src/redux/reducers/user.reducer.js
@@ -18,20 +18,32 @@ export const user = (state = initialState, action) => {
         userToken: action.payload,
         isDisplayControlPanel: true,
         isDisplayRegAndLoginPanel: false,
+        id: state.id,
+        email: state.email,
+        full_name: state.full_name,
+        avatar: state.avatar,
+        is_teacher: state.is_teacher
       }
     case DELETE_USER_TOKEN:
       return {
         userToken: '',
         isDisplayControlPanel: false,
         isDisplayRegAndLoginPanel: true,
+        email: state.email,
+        full_name: state.full_name,
+        avatar: state.avatar,
+        is_teacher: state.is_teacher
       }
     case SET_USER_INFO:
       return {
-       id: action.payload.id,
-       email: action.payload.email,
-       full_name: action.payload.full_name,
-       avatar: action.payload.avatar,
-       is_teacher: action.payload.is_teacher
+        id: action.payload.id,
+        email: action.payload.email,
+        full_name: action.payload.full_name,
+        avatar: action.payload.avatar,
+        is_teacher: action.payload.is_teacher,
+        userToken: state.userToken,
+        isDisplayControlPanel: state.isDisplayControlPanel,
+        isDisplayRegAndLoginPanel: state.isDisplayRegAndLoginPanel
       }
     default:
       return state
